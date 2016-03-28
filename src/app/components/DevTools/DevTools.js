d6842ec33c627bc084cbce7b8243080dc389691f
Jacob Gillespie <jacobwgillespie@gmail.com>
2016-03-28 12:55:50 -0500

    Restrict playback time events in debug tools to verbose mode

--- src/app/components/DevTools/DevTools.js
+++ src/app/components/DevTools/DevTools.js
@@ -1,10 +1,18 @@
 import { createDevTools } from 'redux-devtools';
 import DockMonitor from 'redux-devtools-dock-monitor';
+import FilterMonitor from 'redux-devtools-filter-actions';
 import LogMonitor from 'redux-devtools-log-monitor';
 import React from 'react';
 
+const VERBOSE = process.argv.includes('--verbose');
+
+// Filter out the playback time updates unless verbosely debugging
+const blacklist = VERBOSE ? [] : ['GPM_CHANGE_PLAYBACK_TIME'];
+
 export default createDevTools(
-  <DockMonitor toggleVisibilityKey="ctrl-h" changePositionKey="ctrl-q">
-    <LogMonitor />
-  </DockMonitor>
+  <FilterMonitor blacklist={blacklist}>
+    <DockMonitor toggleVisibilityKey="ctrl-h" changePositionKey="ctrl-q">
+      <LogMonitor />
+    </DockMonitor>
+  </FilterMonitor>
 );
